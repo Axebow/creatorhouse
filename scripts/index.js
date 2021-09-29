367ee347144fcd958df4c6a20354233d125b2d66
Yash Sandeep Kadam <yashk398@gmail.com>
2021-09-29 10:41:17 +0530

    side circles to navigate

--- scripts/index.js
+++ scripts/index.js
@@ -25,3 +25,8 @@ if(localStorage.getItem('theme')!==undefined){
             document.getElementById('theme-container').innerHTML="Switch to dark";
         }
     }
+
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+      return new bootstrap.Popover(popoverTriggerEl)
+})
